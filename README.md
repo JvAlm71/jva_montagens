da584642e7bdd93d267eaaf8e9e0ab587ef16aeb
JvAlm71 <victoralmeida71@usp.br>
2026-02-03 17:09:39 -0300

    feat: inciando repo, para inicio do sistema.

---
+++ README.md
@@ -0,0 +1,7 @@
+# JVA Montagens
+
+Sistema completo para gerenciamento da JVA Montagens. 
+
+Backend: JAVA + Spring-Boot
+
+Frontend: Javascript + NextJS
---
+++ backend/src/test/java/dev/java10x/jva_Montagens/JvaMontagensApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.jva_Montagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvaMontagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
